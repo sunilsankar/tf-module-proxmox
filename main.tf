f5b76a889c4a8c7a392c5ba8b2e73b0e30d8c2b2
Sunil Sankar <nagioskeeper@gmail.com>
2026-02-04 09:07:18 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,6 @@ resource "proxmox_vm_qemu" "vm" {
     tags = join(",", var.tags)
     # -- boot process
     start_at_node_boot = true
-    startup_shutdown = ""
     automatic_reboot = true  # refuse auto-reboot when changing a setting
     cpu {
     cores = var.cores
